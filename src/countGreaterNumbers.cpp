407c0fe6040b66bb2a2b2f4148a754e4c33724b9
ramyakonkala <ramyakonkala@missionrnd.com>
2016-01-29 19:32:34 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -14,12 +14,53 @@ ERROR CASES: Return NULL for invalid inputs.
 NOTES:
 */
 
-struct transaction {
+#include<stdio.h>
+struct transaction
+{
 	int amount;
 	char date[11];
 	char description[20];
 };
 
-int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
-	return -1;
+int countGreaterNumbers(struct transaction *Arr, int len, char *date)
+{
+	int day, month, year, arr_day, arr_month, arr_year;
+	day = date[0] * 10 + date[1];
+	month = date[3] * 10 + date[4];
+	year = date[6] * 1000 + date[7] * 100 + date[8] * 10 + date[9];
+	int i = 0, count = 0;
+	while (i < len)
+	{
+		arr_year = Arr[i].date[6] * 1000 + Arr[i].date[7] * 100 + Arr[i].date[8] * 10 + Arr[i].date[9];
+		if (arr_year > year)
+		{
+			count++;
+			i++;
+		}
+		else if (arr_year == year)
+		{
+			arr_month = Arr[i].date[3] * 10 + Arr[i].date[4];
+			if (arr_month > month)
+			{
+				count++;
+				i++;
+			}
+			else if (arr_month == month)
+			{
+				arr_day = Arr[i].date[0] * 10 + Arr[i].date[1];
+				if (arr_day > day)
+				{
+					count++;
+					i++;
+				}
+				else
+					i++;
+			}
+			else
+				i++;
+		}
+		else
+			i++;
+	}
+	return count;
 }
